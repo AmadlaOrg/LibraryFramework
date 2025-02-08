95adc5b6fec554b032f444e23d18947622997950
jnbdz <237008+jnbdz@users.noreply.github.com>
2025-02-07 22:24:34 -0500

    Updated packages and added documentation in the code.

--- cli/cli.go
+++ cli/cli.go
@@ -8,7 +8,25 @@ import (
 
 type Setup func(*cobra.Command)
 
-// New
+// New attaches to the cli the basic details and command that all the cli application require to work
+//
+// Params:
+// - 📇 name - Is the of the application (normally all lowercase)
+// - 📜 title - Is the name but with uppercase letters where need be
+// - 🚀 setup - Is a callback/decoration-pattern so that other commands can be attached
+//
+// Example:
+//
+//	func main() {
+//		cli.New(
+//			"hery",
+//			"HERY",
+//			"1.0.0",
+//			func(rootCmd *cobra.Command) {
+//				rootCmd.AddCommand(cmd.SettingsCmd)
+//				rootCmd.AddCommand(cmd.CollectionCmd)
+//			})
+//	}
 func New(name, title, version string, setup Setup) {
 
 	appName := fmt.Sprintf("auditor-%s", name)
